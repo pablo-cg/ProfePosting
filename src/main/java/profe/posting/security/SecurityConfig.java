c922cde8f4d5926895f1b9e2cb66627bdb5ae337
pablo-cg <pablo.cuevas08@inacapmail.cl>
2021-07-26 18:18:02 -0400

    Cambios: + Security config + Custom access denied handler + xmlns:sec en login % checkbox para recordar usuario

--- src/main/java/profe/posting/security/SecurityConfig.java
+++ src/main/java/profe/posting/security/SecurityConfig.java
@@ -1,4 +1,63 @@
 package profe.posting.security;
 
-public class SecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import profe.posting.config.CustomAccessDeniedHandler;
+import profe.posting.security.model.UserDetailsServiceImpl;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AccessDeniedHandler accessDeniedHandler(){
+        return new CustomAccessDeniedHandler();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/","/index", "/error", "/fragments", "forbbiden","/login", "/usuario/registro", "/css/**","/js/**","/img/**")
+                .permitAll()
+                .antMatchers("/usuario/registrar").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginProcessingUrl("/signIn")
+                .loginPage("/login").permitAll()
+                .defaultSuccessUrl("/index")
+                .usernameParameter("correo")
+                .passwordParameter("contrasena")
+                .and()
+                .exceptionHandling().accessDeniedHandler(accessDeniedHandler())
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout").permitAll()
+                .deleteCookies("JESSIONID")
+                .and()
+                .rememberMe().tokenValiditySeconds(900).key("profeposting").rememberMeParameter("checkRecuerdame");
+    }
 }
