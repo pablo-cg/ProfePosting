93c5f8cccde6453d9d60caca75d3d59e8b68f8ed
Pablo <cuevasgpablo@gmail.com>
2021-07-28 16:24:02 -0400

    Cambios: + metodos en clase DatabaseCmd para crear roles y usuario admin

--- src/main/java/profe/posting/config/DatabaseCmd.java
+++ src/main/java/profe/posting/config/DatabaseCmd.java
@@ -2,10 +2,16 @@ package profe.posting.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import profe.posting.enums.RolNombre;
 import profe.posting.model.Rol;
+import profe.posting.model.Usuario;
 import profe.posting.service.RolService;
+import profe.posting.service.UsuarioService;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class DatabaseCmd implements CommandLineRunner {
@@ -13,11 +19,38 @@ public class DatabaseCmd implements CommandLineRunner {
     @Autowired
     RolService rolService;
 
+    @Autowired
+    UsuarioService usuarioService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     @Override
     public void run(String... args) throws Exception {
-//        Rol rolAlumno = new Rol(RolNombre.ROLE_ALUMNO);
-//        Rol rolProfesor = new Rol(RolNombre.ROLE_PROFESOR);
-//        rolService.guardar(rolAlumno);
-//        rolService.guardar(rolProfesor);
+//        crearRoles();
+//        crearUsuarioAdmin();
+    }
+
+    void crearRoles(){
+        Rol rolAlumno = new Rol(RolNombre.ROLE_ALUMNO);
+        Rol rolProfesor = new Rol(RolNombre.ROLE_PROFESOR);
+        rolService.guardar(rolAlumno);
+        rolService.guardar(rolProfesor);
+    }
+
+    void crearUsuarioAdmin(){
+        Usuario usuario = new Usuario();
+        String contrasena = passwordEncoder.encode("admin");
+        usuario.setNombre("admin");
+        usuario.setApellidoPaterno("1");
+        usuario.setCorreo("admin@profeposting.cl");
+        usuario.setContrasena(contrasena);
+        Rol rolProfe = rolService.obtenerPorRolNombre(RolNombre.ROLE_PROFESOR).get();
+        Rol rolAlumno = rolService.obtenerPorRolNombre(RolNombre.ROLE_ALUMNO).get();
+        Set<Rol> roles = new HashSet<>();
+        roles.add(rolProfe);
+        roles.add(rolAlumno);
+        usuario.setRoles(roles);
+        usuarioService.crearUsuario(usuario);
     }
 }
